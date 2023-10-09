3fec9ae41cf69e94bcddb9ff5dc36d37ba337654
Adrian Pionka <adrian@pionka.com>
2023-10-09 20:05:41 +0200

    add dynamic app version property

--- src/test/java/info/pionas/ideas/ApplicationTests.java
+++ src/test/java/info/pionas/ideas/ApplicationTests.java
@@ -1,13 +1,19 @@
 package info.pionas.ideas;
 
+import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
 class ApplicationTests {
 
+    @Value("${info.app.version}")
+    private String appVersion;
+
     @Test
     void contextLoads() {
+        Assertions.assertEquals("0.0.1-SNAPSHOT", appVersion);
     }
 
 }
